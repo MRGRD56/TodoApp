fe1e9c7c4d99e9a9ae10dadf0381030df657383f
MRGRD56 <kirgol85@gmail.com>
2021-08-11 14:52:01 +0500

    add methods to call the API

---
+++ TodoApp.DesktopClient/ViewModels/PagesViewModels/HomePageViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoApp.Infrastructure.Models;
+
+namespace TodoApp.DesktopClient.ViewModels.PagesViewModels
+{
+    public class HomePageViewModel : ViewModel
+    {
+        public ObservableCollection<TodoItem> TodoItems { get; } = new();
+
+        public HomePageViewModel()
+        {
+            Initialize();
+        }
+
+        private async void Initialize()
+        {
+            await LoadTodoItemsAsync();
+        }
+
+        private async Task LoadTodoItemsAsync()
+        {
+            
+        }
+    }
+}
