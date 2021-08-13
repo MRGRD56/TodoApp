526b79c551b09c6390811fb20ab8c8e8ab7e1e93
MRGRD56 <kirgol85@gmail.com>
2021-08-13 12:33:34 +0500

    default cursor if IsEditMode

---
+++ TodoApp.DesktopClient/Converters/IsHandCursorConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace TodoApp.DesktopClient.Converters
+{
+    public class IsHandCursorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var isHandCursor = (bool)value;
+            return isHandCursor ? Cursors.Hand : null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
