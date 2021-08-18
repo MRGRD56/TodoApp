58fdac0c5a44b0d3c2c17105f68a5a8f17ef29c5
MRGRD56 <kirgol85@gmail.com>
2021-08-18 13:22:02 +0500

    copy files from the old mobile project

---
+++ TodoApp.MobileClient/TodoApp.MobileClient.Android/Models/AndroidLocalDbFileDirectoryProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using TodoApp.MobileClient.Droid.Models;
+using TodoApp.MobileClient.Models;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(AndroidLocalDbFileDirectoryProvider))]
+namespace TodoApp.MobileClient.Droid.Models
+{
+    public class AndroidLocalDbFileDirectoryProvider : ILocalDbFileDirectoryProvider
+    {
+        public string GetLocalDbFileDirectory() => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal));
+    }
+}
