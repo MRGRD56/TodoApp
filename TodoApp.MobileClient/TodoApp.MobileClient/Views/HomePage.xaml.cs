34c9e8729c43623a851e30b4395808c1c7dd8d52
MRGRD56 <kirgol85@gmail.com>
2021-08-19 10:13:03 +0500

    bugs in the Xamarin.Forms app

--- TodoApp.MobileClient/TodoApp.MobileClient/Views/HomePage.xaml.cs
+++ TodoApp.MobileClient/TodoApp.MobileClient/Views/HomePage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using TodoApp.MobileClient.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +12,19 @@ namespace TodoApp.MobileClient.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class HomePage : ContentPage
     {
+        private HomePageViewModel ViewModel => (HomePageViewModel)BindingContext;
+
         public HomePage()
         {
             InitializeComponent();
+        }
+
+        private void ScrollViewOnScrolled(object sender, ScrolledEventArgs e)
+        {
+            var scrollView = (ScrollView)sender;
+            var offset = e.ScrollY;
+            var height = scrollView.ContentSize.Height - scrollView.Height;
+            ViewModel.OnTodoItemsScroll(offset, height);
         }
     }
 }
